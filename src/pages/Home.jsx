52fd50f9aa984062502aaf442dad59ca2d9b5bc6
Alberto Candido <albertocandido98@gmail.com>
2021-08-15 15:42:34 -0300

    Finaliza header do site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import Navbar from "../components/Navbar"
+// import Header from "../components/Header"
+import NavBar from "../components/NavBar"
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <NavBar />
     </>
   )
 }
