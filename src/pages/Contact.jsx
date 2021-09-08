4b629fb627bc0e9fe6ba999a5722a4d79f67e046
Alberto Candido <albertocandido98@gmail.com>
2021-09-08 00:23:30 -0300

    Adiciona icones de redes com animaçao base em codigo do code-architects

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,54 @@
 import React from 'react'
-// import { send } from 'emailjs-com';
+import { send, init } from 'emailjs-com';
+import '../styles/contact.css';
+import instagram from '../img/instagram.svg';
+import gitHub from '../img/gitHub.svg';
+import linkedIn from '../img/linkedIn.svg';
+import twitter from '../img/twitter.svg';
+import spotify from '../img/spotify.svg';
+
 
 const Contact = () => {
-  const [toSend, setToSend] = React.useState({
+  const initialState = {
     from_name: '',
+    to_name: 'Alberto',
     message: '',
     reply_to: '',
-  });
+  }
+
+  const [toSend, setToSend] = React.useState(initialState);
 
   const onSubmit = (e) => {
+    init("user_mdrJJlYqofmzWIFqxmIws");
     e.preventDefault();
-    e.preventDefault();
-    // send(
-    //   'SERVICE ID',
-    //   'TEMPLATE ID',
-    //   toSend,
-    //   'User ID'
-    // )
-    //   .then((response) => {
-    //     console.log('SUCCESS!', response.status, response.text);
-    //   })
-    //   .catch((err) => {
-    //     console.log('FAILED...', err);
-    //   });
+
+    console.log('entrei');
+    if (toSend.from_name.trim() === '') {
+      alert('Please enter your name');
+      return;
+    }
+    if (toSend.reply_to.trim() === '') {
+      alert('Please enter your email');
+      return;
+    }
+    if (toSend.message.trim() === '') {
+      alert('Please enter your message');
+      return;
+    }
+  
+    send(
+      'service_a70vrtw',
+      'template_ou98d82',
+      toSend,
+      'user_mdrJJlYqofmzWIFqxmIws'
+    )
+      .then(() => {
+        alert('Your message has been sent!');
+        setToSend(initialState);
+      })
+      .catch(() => {
+        alert('An error has occurred');
+      });
   };
 
   const handleChange = (e) => {
@@ -30,33 +56,43 @@ const Contact = () => {
   };
 
   return (
-    <main>
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        name="from_name"
-        placeholder="Seu nome"
-        value={toSend.from_name}
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="message"
-        placeholder="Sua Mensagem"
-        value={ toSend.message }
-        onChange={ handleChange }
-      />
-      <input
-        type="text"
-        name="reply_to"
-        placeholder="Seu email"
-        value={ toSend.reply_to }
-        onChange={ handleChange }
-      />
-      <button type="submit"> 
-        Enviar
-      </button>
-    </form>
+    <main className="main-contact">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="from_name"
+          placeholder="Seu nome"
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="Sua Mensagem"
+          value={ toSend.message }
+          onChange={ handleChange }
+        />
+        <input
+          type="text"
+          name="reply_to"
+          placeholder="Seu email"
+          value={ toSend.reply_to }
+          onChange={ handleChange }
+        />
+        <button type="submit" onClick={ onSubmit }> 
+          Enviar
+        </button>
+      </form>
+      {/* animação de redes sociais usando base https://code-architects.github.io/social-media-icons/ */}
+      <div class="wrapper">
+          <div class="socialicons">
+              <a href="https://github.com/albertoscandido" title="GitHub" class="icon"><img src={ gitHub } alt="GitHub" /></a>
+              <a href="https://www.linkedin.com/in/albertocandido/" title="LinkedIn" class="icon"><img src={ linkedIn } alt="LinkedIn" /></a>
+              <a href="https://www.instagram.com/albertocandido98/" title="Instagram" class="icon"><img src={ instagram } alt="Instagram" /></a>
+              <a href="https://www.instagram.com/albertocandido98/" title="Twitter" class="icon"><img src={ twitter } alt="twitter"/></a>
+              <a href="https://www.instagram.com/albertocandido98/" title="Spotify" class="icon"><img src={ spotify } alt="spotify"/></a>
+          </div>
+      </div>
     </main>
   );
 }
